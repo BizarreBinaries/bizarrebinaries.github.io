fc8c35f73fae930c1ca9df6178037ffe5f1ea9cb
BizarreBinaries <bizarrebinary@gmail.com>
2025-05-29 23:16:57 +1000

    commit - new post

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1748171652',resources: [
+    cacheName: 'chirpy-1748524539',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
