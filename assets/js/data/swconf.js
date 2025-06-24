92bdd94962d6f100b6d4a3bae1513a466cb64e75
BizarreBinaries <bizarrebinary@gmail.com>
2025-06-24 13:45:03 +1000

    commit - theme change

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1748526073',resources: [
+    cacheName: 'chirpy-1750736654',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
